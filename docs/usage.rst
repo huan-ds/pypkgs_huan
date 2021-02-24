ed708144bf3f34e859f9c916dedd46292e05c159
lihh <lihuanhuan1003@hotmail.com>
2021-02-24 10:05:17 -0800

    initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs-huan in a project::
+
+    import pypkgs_huan
---
+++ tests/test_pypkgs_huan.py
@@ -0,0 +1,5 @@
+from pypkgs_huan import __version__
+from pypkgs_huan import pypkgs_huan
+
+def test_version():
+    assert __version__ == '0.1.0'
