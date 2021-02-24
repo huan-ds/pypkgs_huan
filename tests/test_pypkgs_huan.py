eafdb027ee9ea95412754f090718067d8d8983d2
lihh <lihuanhuan1003@hotmail.com>
2021-02-24 11:44:11 -0800

    added unit tests for catbind

--- tests/test_pypkgs_huan.py
+++ tests/test_pypkgs_huan.py
@@ -1,5 +1,13 @@
 from pypkgs_huan import __version__
 from pypkgs_huan import pypkgs_huan
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgs_huan.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgs_huan.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
